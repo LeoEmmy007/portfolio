b6a6b91405b16fb57a57cfb88600e045dd67ef83
leo <leoemmy007@gmail.com>
2025-10-28 09:57:57 -0800

    added new files successfully

--- js/script.js
+++ js/script.js
@@ -28,12 +28,7 @@ document.getElementById("contactForm").addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const form = e.target;
-  const payload = {
-    name: form.name.value,
-    email: form.email.value,
-    subject: form.subject.value,
-    message: form.message.value,
-  };
+  const formData = new FormData(form);
 
   const url =
     "https://script.google.com/macros/s/AKfycbxEmssc2Zvl_B2TuQndoseo4m4kQRi9cLomUvK3yTYvEnqwQuuC3BWuSOGu6Zosk1fH/exec";
@@ -41,9 +36,7 @@ document.getElementById("contactForm").addEventListener("submit", async (e) => {
   try {
     await fetch(url, {
       method: "POST",
-      mode: "no-cors", // avoids CORS blocking
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(payload),
+      body: formData,
     });
 
     alert("✅ Message sent successfully!");
